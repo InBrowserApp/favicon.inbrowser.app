2d578ee815cf47361feca7cc518d6a6447ef1ffc
Seedgou <i@zczc.cz>
2023-10-31 16:36:54 +0800

    uploading image

---
+++ src/components/tools/favicon-generator/FaviconGenerator.vue
@@ -0,0 +1,13 @@
+<template>
+  <ImageUpload @update:file="image = $event" />
+  <img :src="imageURL" />
+</template>
+
+<script setup lang="ts">
+import ImageUpload from "./ImageUpload.vue";
+import { ref } from "vue";
+import { useObjectUrl } from "@vueuse/core";
+
+const image = ref<Blob | undefined>(undefined);
+const imageURL = useObjectUrl(image);
+</script>
--- src/views/FaviconGeneratorView.vue
+++ src/views/FaviconGeneratorView.vue
@@ -2,6 +2,7 @@
   <main>
     <BackToHome />
     <n-h2 prefix="bar" align-text>Favicon Generator</n-h2>
+    <FaviconGenerator />
   </main>
 </template>
 
@@ -9,6 +10,7 @@
 import { NH2 } from "naive-ui";
 import { useHead } from "@vueuse/head";
 import BackToHome from "@/components/misc/BackToHome.vue";
+import FaviconGenerator from "@/components/tools/favicon-generator/FaviconGenerator.vue";
 
 useHead({
   title: "Favicon Generator | Favicon InBrowser.App",
