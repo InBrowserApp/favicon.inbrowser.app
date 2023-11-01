f8878058518ab8d5ef981c9c67b1b31cac4d96de
Seedgou <i@zczc.cz>
2023-11-01 10:11:27 +0800

    preprocess image to center and square

--- src/components/tools/favicon-generator/FaviconGenerator.vue
+++ src/components/tools/favicon-generator/FaviconGenerator.vue
@@ -1,13 +1,20 @@
 <template>
   <ImageUpload @update:file="image = $event" />
   <img :src="imageURL" />
+  <img :src="preprocessedImageURL" />
 </template>
 
 <script setup lang="ts">
 import ImageUpload from "./ImageUpload.vue";
 import { ref } from "vue";
-import { useObjectUrl } from "@vueuse/core";
+import { useObjectUrl, computedAsync } from "@vueuse/core";
+import { preprocessImage } from "@/utils/image/preprocess-image";
 
 const image = ref<Blob | undefined>(undefined);
 const imageURL = useObjectUrl(image);
+const preprocessedImage  = computedAsync(async () => {
+  if (image.value === undefined) return undefined;
+  return await preprocessImage(image.value);
+});
+const preprocessedImageURL = useObjectUrl(preprocessedImage);
 </script>
