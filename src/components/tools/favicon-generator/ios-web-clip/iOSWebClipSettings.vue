e6b34afc285e503ca35659b319ecd205f48cf0c8
Seedgou <i@zczc.cz>
2023-11-01 18:52:39 +0800

    add settings title

--- src/components/tools/favicon-generator/ios-web-clip/iOSWebClipSettings.vue
+++ src/components/tools/favicon-generator/ios-web-clip/iOSWebClipSettings.vue
@@ -1,19 +1,20 @@
 <template>
   <n-tabs type="line" animated>
     <n-tab-pane name="background" tab="Background" display-directive="show">
+      <div style="margin-bottom: 0.25em">Background Color</div>
       <n-color-picker
         :show-alpha="false"
         v-model:value="options.backgroundColor"
         :modes="['hex']"
       />
+      <div style="margin-bottom: 0.25em">Margin</div>
       <n-slider v-model:value="options.margin" :step="1" />
-      {{ options.margin }}
     </n-tab-pane>
   </n-tabs>
 </template>
 
 <script setup lang="ts">
-import { NTabs, NTabPane, NColorPicker, NSlider } from "naive-ui";
+import { NTabs, NTabPane, NColorPicker, NSlider, NText } from "naive-ui";
 import type { iOSWebClipOptions } from "@/utils/favicon-generator/ios-web-clip";
 import { useVModel } from "@vueuse/core";
 
