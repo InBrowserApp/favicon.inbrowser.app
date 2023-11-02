add6f9ee1ded2ff75219ebbdb99f520b03b5a708
Seedgou <i@zczc.cz>
2023-11-02 16:42:01 +0800

    add chrome tab preview

--- src/components/tools/favicon-generator/desktop-browser/DesktopBrowserPreview.vue
+++ src/components/tools/favicon-generator/desktop-browser/DesktopBrowserPreview.vue
@@ -5,29 +5,25 @@
       :options="options"
       :generalInfoOptions="generalInfoOptions"
     />
+    <n-divider />
+    <ChromeTabPreview
+      :image="image"
+      :options="options"
+      :generalInfoOptions="generalInfoOptions"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { useObjectUrl } from "@vueuse/core";
+import { NDivider } from "naive-ui";
 import type { DesktopBrowserOptions } from "@/utils/favicon-generator/desktop-browser";
-import { computed } from "vue";
 import type { GeneralInfoOptions } from "@/utils/favicon-generator/general-info";
 import GoogleSearchResult from "./GoogleSearchResult.vue";
+import ChromeTabPreview from "./ChromeTabPreview.vue";
 
-const props = defineProps<{
+defineProps<{
   image: Blob | undefined;
   options: DesktopBrowserOptions;
   generalInfoOptions: GeneralInfoOptions;
 }>();
-
-const image = computed<Blob | undefined>(() => {
-  if (props.options.image) {
-    return props.options.image;
-  } else {
-    return props.image;
-  }
-});
-
-const imageURL = useObjectUrl(image);
 </script>
