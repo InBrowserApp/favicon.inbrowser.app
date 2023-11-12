512ba693afae5fbe9219b59baa80c07e2a2e1648
Seedgou <i@zczc.cz>
2023-11-12 21:26:57 +0800

    update general info input

--- src/components/tools/favicon-generator/general-info/GeneralInfo.vue
+++ src/components/tools/favicon-generator/general-info/GeneralInfo.vue
@@ -7,15 +7,63 @@
       </n-form-item>
     </n-grid-item>
     <n-grid-item :span="2">
-      <n-form-item label="App Short Name">
+      <n-form-item label="Short Name">
         <n-input v-model:value="options.short_name" />
+      </n-form-item>
+    </n-grid-item>
+    <n-grid-item :span="4">
+      <n-form-item label="Description">
+        <n-input v-model:value="options.description" />
+      </n-form-item>
+    </n-grid-item>
+    <n-grid-item :span="2">
+      <n-form-item label="Theme Color">
+        <n-color-picker
+          :show-alpha="false"
+          v-model:value="options.theme_color"
+          :modes="['hex']"
+        />
+      </n-form-item>
+    </n-grid-item>
+    <n-grid-item :span="2">
+      <n-form-item label="Background Color">
+        <n-color-picker
+          :show-alpha="false"
+          v-model:value="options.background_color"
+          :modes="['hex']"
+        />
+      </n-form-item>
+    </n-grid-item>
+
+    <n-grid-item :span="2">
+      <n-form-item label="Start URL">
+        <n-input v-model:value="options.start_url" />
+      </n-form-item>
+    </n-grid-item>
+    <n-grid-item :span="2">
+      <n-form-item label="Path of icons">
+        <n-input v-model:value="options.path" />
+      </n-form-item>
+    </n-grid-item>
+
+    <n-grid-item :span="2">
+      <n-form-item label="Display">
+        <n-select v-model:value="options.display" :options="displayOptions" />
       </n-form-item>
     </n-grid-item>
   </n-grid>
 </template>
 
 <script setup lang="ts">
-import { NH2, NInput, NFormItem, NGrid, NGridItem } from "naive-ui";
+import {
+  NH2,
+  NInput,
+  NFormItem,
+  NGrid,
+  NGridItem,
+  NColorPicker,
+  NSelect,
+} from "naive-ui";
 import type { GeneralInfoOptions } from "@/utils/favicon-generator/general-info";
 import { useVModel } from "@vueuse/core";
 
@@ -26,4 +74,11 @@ const props = defineProps<{
 const emit = defineEmits(["update:options"]);
 
 const options = useVModel(props, "options", emit);
+
+const displayOptions = [
+  { label: "Fullscreen", value: "fullscreen" },
+  { label: "Standalone", value: "standalone" },
+  { label: "Minimal UI", value: "minimal-ui" },
+  { label: "Browser", value: "browser" },
+];
 </script>
