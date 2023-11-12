eb09fd3ebf52421a1057be19ad3ca9fd9214d195
Seedgou <i@zczc.cz>
2023-11-13 00:05:43 +0800

    add link of difference between any and maskable

--- src/components/tools/favicon-generator/pwa/PWA.vue
+++ src/components/tools/favicon-generator/pwa/PWA.vue
@@ -3,6 +3,21 @@
     <n-icon :component="LogoPwa" style="vertical-align: -0.15em" />
     Progressive Web App (PWA)
   </n-h2>
+  <n-p>
+    <n-button
+      text
+      tag="a"
+      href="https://web.dev/articles/maskable-icon"
+      target="_blank"
+      style="vertical-align: -0.15em"
+    >
+      <template #icon>
+        <n-icon :component="BookInformation20Regular" />
+      </template>
+      What's the difference between &nbsp;<n-text code>"purpose": "any"</n-text
+      >&nbsp; and &nbsp;<n-text code>"purpose": "maskable"</n-text>?
+    </n-button>
+  </n-p>
   <n-h3 prefix="bar" align-text>
     PWA <n-text code>"purpose": "any"</n-text>
   </n-h3>
@@ -36,7 +51,16 @@
 </template>
 
 <script setup lang="ts">
-import { NH2, NH3, NText, NGrid, NGridItem, NIcon } from "naive-ui";
+import {
+  NH2,
+  NH3,
+  NText,
+  NGrid,
+  NGridItem,
+  NIcon,
+  NP,
+  NButton,
+} from "naive-ui";
 import type { PWAOptions } from "@/utils/favicon-generator/pwa";
 import { useVModel } from "@vueuse/core";
 import type { GeneralInfoOptions } from "@/utils/favicon-generator/general-info";
@@ -45,6 +69,7 @@ import PWAMaskablePreview from "./maskable/PWAMaskablePreview.vue";
 import PWASettings from "./any/PWASettings.vue";
 import PWAMaskableSettings from "./maskable/PWAMaskableSettings.vue";
 import { LogoPwa } from "@vicons/ionicons5";
+import { BookInformation20Regular } from "@vicons/fluent";
 
 const props = defineProps<{
   image: Blob | undefined;
