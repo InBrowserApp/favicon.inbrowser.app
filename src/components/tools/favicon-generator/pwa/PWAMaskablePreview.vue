c8e3c9df32ca6d8a32bee13939bab08cdf37e014
Seedgou <i@zczc.cz>
2023-11-12 15:55:01 +0800

    split maskable preview

--- src/components/tools/favicon-generator/pwa/PWAMaskablePreview.vue
+++ src/components/tools/favicon-generator/pwa/PWAMaskablePreview.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <PWAPreviewWindowsTaskbar :image="image" :options="props.options" />
+    <PWAPreviewAndroid :image="image" :options="props.options" />
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 import type { PWAOptions } from "@/utils/favicon-generator/pwa";
 import type { GeneralInfoOptions } from "@/utils/favicon-generator/general-info";
 import { computed } from "vue";
-import PWAPreviewWindowsTaskbar from "./PWAPreviewWindowsTaskbar.vue";
+import PWAPreviewAndroid from "./PWAPreviewAndroid.vue";
 
 const props = defineProps<{
   image: Blob | undefined;
