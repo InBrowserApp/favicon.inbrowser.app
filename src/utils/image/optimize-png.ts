17ad7e3d92283fcf455d0900fbb353f5d556d598
Seedgou <i@zczc.cz>
2023-11-12 15:22:11 +0800

    remove optimizePNG temporarily

--- src/utils/image/optimize-png.ts
+++ src/utils/image/optimize-png.ts
@@ -1,15 +1,19 @@
-import { optimise } from "@/utils/packages/oxipng";
+// import { optimise } from "@/utils/packages/oxipng";
 
 export async function optimizePNG(blob: Blob) {
-  if (blob.type !== "image/png") {
-    throw new Error("Blob is not a PNG");
-  }
+  // if (blob.type !== "image/png") {
+  //   throw new Error("Blob is not a PNG");
+  // }
 
-  const arraybuffer = await blob.arrayBuffer();
-  const optimisedArrayBuffer = await optimise(arraybuffer);
-  const optimisedBlob = new Blob([optimisedArrayBuffer], {
-    type: blob.type,
-  });
+  // const arraybuffer = await blob.arrayBuffer();
+  // const optimisedArrayBuffer = await optimise(arraybuffer);
+  // const optimisedBlob = new Blob([optimisedArrayBuffer], {
+  //   type: blob.type,
+  // });
 
-  return optimisedBlob;
+  // return optimisedBlob;
+
+  // FIXME: vite build fails with the above code, so we'll just return the original blob for now
+  // https://github.com/vitejs/vite/issues/13367
+  return blob;
 }
