a9df6619c994916012337a6684307952552bc8c3
Seedgou <i@zczc.cz>
2023-11-12 16:03:53 +0800

    update pwa heading

--- src/components/tools/favicon-generator/pwa/PWA.vue
+++ src/components/tools/favicon-generator/pwa/PWA.vue
@@ -1,6 +1,8 @@
 <template>
   <n-h2 prefix="bar" align-text>PWA</n-h2>
-  <n-h3 prefix="bar" align-text>PWA</n-h3>
+  <n-h3 prefix="bar" align-text>
+    PWA <n-text code>"purpose": "any"</n-text>
+  </n-h3>
   <n-grid x-gap="12" :cols="5">
     <n-grid-item :span="2">
       <PWAPreview
@@ -13,7 +15,9 @@
       <PWASettings :image="image" v-model:options="options" />
     </n-grid-item>
   </n-grid>
-  <n-h3 prefix="bar" align-text>PWA Maskable</n-h3>
+  <n-h3 prefix="bar" align-text>
+    PWA <n-text code>"purpose": "maskable"</n-text>
+  </n-h3>
   <n-grid x-gap="12" :cols="5">
     <n-grid-item :span="2">
       <PWAMaskablePreview
@@ -29,7 +33,7 @@
 </template>
 
 <script setup lang="ts">
-import { NH2, NH3, NGrid, NGridItem } from "naive-ui";
+import { NH2, NH3, NText, NGrid, NGridItem } from "naive-ui";
 import type { PWAOptions } from "@/utils/favicon-generator/pwa";
 import { useVModel } from "@vueuse/core";
 import type { GeneralInfoOptions } from "@/utils/favicon-generator/general-info";
