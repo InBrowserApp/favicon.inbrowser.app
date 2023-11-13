b7f4d95c8056e37a320d4877091fc516be869981
Seedgou <i@zczc.cz>
2023-11-13 10:07:25 +0800

    update image upload text and icon

--- src/components/tools/favicon-generator/ImageUpload.vue
+++ src/components/tools/favicon-generator/ImageUpload.vue
@@ -3,17 +3,17 @@
     <n-upload-dragger>
       <div style="margin-bottom: 12px">
         <n-icon size="48" :depth="3">
-          <archive-icon />
+          <ImageEdit24Regular />
         </n-icon>
       </div>
-      <n-text style="font-size: 16px">Click or drag to upload Image</n-text>
+      <n-text style="font-size: 16px">Click or drag to select image</n-text>
     </n-upload-dragger>
   </n-upload>
 </template>
 
 <script setup lang="ts">
 import { NUpload, NUploadDragger, NIcon, NText } from "naive-ui";
-import ArchiveIcon from "@vicons/ionicons5/ArchiveOutline";
+import { ImageEdit24Regular } from "@vicons/fluent";
 import type { UploadFileInfo } from "naive-ui";
 import { useMessage } from "naive-ui";
 
