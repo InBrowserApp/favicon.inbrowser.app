687e28eca205ce2dad2f39a278aa8c99516a6a7b
Seedgou <i@zczc.cz>
2023-11-14 09:36:06 +0800

    disable inlineStyles for now

--- src/utils/favicon-generator/desktop-browser/generate-favicon-svg.ts
+++ src/utils/favicon-generator/desktop-browser/generate-favicon-svg.ts
@@ -4,9 +4,9 @@ export async function generateFaviconSVG(
   blob: Blob,
   options: DesktopBrowserOptions
 ): Promise<Blob> {
-  // FIXME: svgo will break css media queries
+  // TODO: svgo will break css media queries
   // https://github.com/svg/svgo/issues/1834
-  // const svgo = import("svgo");
+  const svgo = import("svgo");
 
   const image = options?.image ?? blob;
   if (image === undefined) {
@@ -17,15 +17,27 @@ export async function generateFaviconSVG(
     throw new Error("image is not svg");
   }
 
-  return image;
-  // const svgString = await image.text();
+  const svgString = await image.text();
 
-  // const optimize = (await svgo).optimize;
-  // const result = optimize(svgString, {});
+  const optimize = (await svgo).optimize;
+  const result = optimize(svgString, {
+    multipass: true,
+    plugins: [
+      {
+        name: "preset-default",
+        params: {
+          overrides: {
+            // customize default plugin options
+            inlineStyles: false,
+          },
+        },
+      },
+    ],
+  });
 
-  // const newBlob = new Blob([result.data], {
-  //   type: "image/svg+xml",
-  // });
+  const newBlob = new Blob([result.data], {
+    type: "image/svg+xml",
+  });
 
-  // return newBlob;
+  return newBlob;
 }
